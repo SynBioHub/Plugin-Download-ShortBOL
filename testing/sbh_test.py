e6430948274462eda506a177c8791fc5617573e7
WillW34 <78357757+WillW34@users.noreply.github.com>
2021-07-08 14:19:30 +0100

    Update sbh_test.py

--- testing/sbh_test.py
+++ testing/sbh_test.py
@@ -17,7 +17,7 @@ data = """{"complete_sbol": "http://localhost:5000/public/plugintest.sbol",
 
 def test():
     save_path = 'out'
-    file_name = "test.txt"
+    file_name = "test.html"
 
     completeName = os.path.join(save_path, file_name)
 
@@ -28,7 +28,7 @@ def test():
     print(run_response.headers)
     print(run_response.status_code)
 
-    file1 = open(completeName, "w")
+    file1 = open(completeName, 'w')
     file1.write(run_response.text)
     file1.close()
 
